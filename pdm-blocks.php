f1c47697a3b77247121ef1503081c24c2d8d0f5d
Max <you@example.com>
2026-04-14 11:55:40 -0600

    updated v

--- pdm-blocks.php
+++ pdm-blocks.php
@@ -5,7 +5,7 @@
  * Description:       A collection of essential PDM blocks.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.3.3
+ * Version:           1.3.4
  * Author:            Performance Driven Marketing
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
