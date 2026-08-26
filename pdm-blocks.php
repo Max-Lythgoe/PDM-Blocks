f2ac64ece86fe19b4275bf6cd9fb45024b89f4fe
Max <you@example.com>
2026-08-26 12:27:19 -0600

    accessibility improvements

--- pdm-blocks.php
+++ pdm-blocks.php
@@ -5,7 +5,7 @@
  * Description:       A collection of essential PDM blocks.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.7.62
+ * Version:           1.8.0
  * Author: Performance Driven Marketing
  * Author URI: https://performancedrivenmarketing.com
  * License:           GPL-2.0-or-later
